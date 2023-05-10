3599ae26ad84f7137a01dd15d9b9604b968325e4
Seoyoon <seoyoon047@gmail.com>
2023-05-10 16:28:15 +0900

    feat: ngnix 외부 파일 설정 추가

--- src/main/java/projectB/meongbti/MeongbtiApplication.java
+++ src/main/java/projectB/meongbti/MeongbtiApplication.java
@@ -2,12 +2,22 @@ package projectB.meongbti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class MeongbtiApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "/app/config/meongbti/real-application.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(MeongbtiApplication.class, args);
+		new SpringApplicationBuilder(MeongbtiApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
+
 	}
 
 }
