219b1d4f85efeb8c8c8358a9f15833eb491b89bd
Seoyoon <seoyoon047@gmail.com>
2023-05-11 22:23:50 +0900

    fix: rds 파일 수정

--- src/main/java/projectB/meongbti/MeongbtiApplication.java
+++ src/main/java/projectB/meongbti/MeongbtiApplication.java
@@ -7,16 +7,12 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
-@EnableScheduling
-@EnableCaching
-@EnableJpaAuditing
 public class MeongbtiApplication {
-
-
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
 			+ "classpath:application.yml,"
-			+ "/app/config/meongbti/real-application.yml";
+			+ "./app/config/meongbti/real-application.yml";
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(MeongbtiApplication.class)
 				.properties(APPLICATION_LOCATIONS)
